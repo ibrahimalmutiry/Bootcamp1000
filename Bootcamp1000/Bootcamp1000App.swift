5a28cbfc9ac43ccde44361687d7789cfa9304989
ibrahim Almutiry <iibrahim.almutiry@gmail.com>
2022-02-06 17:13:51 +0300

    Initial Commit

---
+++ Bootcamp1000/Bootcamp1000App.swift
@@ -0,0 +1,17 @@
+//
+//  Bootcamp1000App.swift
+//  Bootcamp1000
+//
+//  Created by ibrahim almutiry on 06/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bootcamp1000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bootcamp1000/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bootcamp1000
+//
+//  Created by ibrahim almutiry on 06/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
